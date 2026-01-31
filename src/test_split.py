d56adb47bac77fad8c9f2d6632060c6a6ac90c93
Krieten <76659461+Krieten@users.noreply.github.com>
2026-01-31 22:35:35 +0100

    add: link- and imageextraction

--- src/test_split.py
+++ src/test_split.py
@@ -1,6 +1,6 @@
 import unittest
 
-from split import split_nodes_delimiter
+from split import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 
 class TestSplit(unittest.TestCase):
@@ -82,6 +82,18 @@ class TestSplit(unittest.TestCase):
             ],
             new_nodes,
         )
+    
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to yahoo](https://yahoo.com) and [to google](https://google.com)"
+        )
+        self.assertListEqual([("to yahoo", "https://yahoo.com"), ("to google", "https://google.com")], matches)
 
 
 if __name__ == "__main__":
